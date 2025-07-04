894c36dad4cd36b3acb1716ea00dce179aef1ac2
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-07-04 19:47:10 +0200

    Select querySelector() and querySelectorAll() methods 1

--- main.js
+++ main.js
@@ -1,7 +1,2 @@
-let element = document.querySelector('*');
-let elements = document.querySelectorAll('*');
-let firstHeading = document.querySelector('h1');
-let heading2 = document.querySelectorAll('h2');
-let note = document.querySelector('.menu-item');
-let notes = document.querySelectorAll('.menu-item');
-let logo = document.querySelector('#logo');
+let notes = document.querySelector(".note");
+console.log(notes);
