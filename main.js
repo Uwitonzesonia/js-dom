07b9e00edbaec4dbafb90362a227d307354b5334
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-07-06 15:03:49 +0200

    select create element method

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-let second = document.querySelector(".second");
-console.log("Next.previousElementSibling");
+
+    let Div = document.createElement("div");
+    Div.innerHTML = "<p> Welcome to Great Stack</p>"; 
+    Document.body.appendChild(Div);
+    console.log(Div);
+
