82f58e5645fc5bef1c6e11b53583fc012f2c4aec
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-08-03 14:28:11 +0200

    Array destructuring

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-let scores = [80, 90, 70];
-
-for (let score of scores) {
-    score = score + 5;
-    console.log(score);
+function getScores() {
+  return [70, 80, 90];
 }
+let scores = getScores();
+
+let x = scores[0],
+  y = scores[1],
+  z = scores[2];
+
+console.log({ x, y, z });
