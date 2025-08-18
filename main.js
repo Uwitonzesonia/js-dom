c2dbcfeba1b53569a570f4b7f9c57616372d1af8
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-08-18 17:04:49 +0200

    change

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-      const person = { name: "Alice", age: 30 };
+  const person = { name: "Alice", age: 30 };
     const { name, age } = person;
-
-    console.log(name);
-
-    const { name: personName, age: personAge } = person;
+   const { name: personName, age: personAge } = person;
     console.log(personName);
+    console.log(personAge);
