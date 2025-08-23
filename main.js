c250014859d9650903bb342ca93f55419a96957d
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-08-23 10:46:33 +0200

    change

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-function printMe() {
-    console.log("print Me");
-}
-function test() {
-    console.log("test");
-}
-setTimeout(printMe, 2000);
-test(); 
+function f1() {
+    console.log('f1');
+} 
+function f2() {
+    console.log('f2');
+} 
+function main() {
+    console.log('main');
+    setTimeout(f1,2000); 
+    f2();
+} 
+main();
   
   
 
