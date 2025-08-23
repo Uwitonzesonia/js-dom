c06cc8b1ed119e78a2c1dd4ff7ebb574f797d8c9
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-08-23 12:26:46 +0200

    change

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 function f1() {
-    console.log('f1');
+ console.log('f1');
 } 
-function f2() {
+function f1() {
     console.log('f2');
+    
 } 
 function main() {
-    console.log('main');
-    setTimeout(f1,2000); 
+    console.log('main')
+    setTimeout(f1 , 0);
+    new Promise(function(resolved,rejected){
+        resolve(I am Promised);
+    }).then(resolve => console.log(resolve)); 
     f2();
-} 
-main();
-  
-  
+}   
+  main();
 
 
  
