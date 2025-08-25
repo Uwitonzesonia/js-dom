9f464e6e55975c3ec83b14f3743f7c1648f41882
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-08-25 16:21:09 +0200

    change

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-function f1() {
- console.log('f1');
-} 
-function f1() {
-    console.log('f2');
-    
-} 
-function main() {
-    console.log('main')
-    setTimeout(f1 , 0);
-    new Promise(function(resolved,rejected){
-        resolve(I am Promised);
-    }).then(resolve => console.log(resolve)); 
-    f2();
-}   
-  main();
+let  getWater = new Promise(function(resolve, reject){
+    // Got the water 
+let value ="water";
+resolve(value);
+});
+
+const grandparentscooking = () => {
+
+    getWater.then(function(result){
+
+    console.log(`cooking with ${result}`)
+
+})
+
+};
+grandparentscooking();
+
+
+
 
 
  
