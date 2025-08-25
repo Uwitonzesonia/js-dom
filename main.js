2c911275d980c037e584c85b7b9bc4d0b14512d9
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-08-26 01:47:12 +0200

    callback

--- main.js
+++ main.js
@@ -7,6 +7,18 @@ foo(function () {
     console.log("bar");
 })
 
+orderPizza('veg','cheesebarbeque',function(msg){
+    console.log(msg);
+});
+function orderPizza(type, name, callback) {
+    console.log(`pizza ${type} ${name} is ordered`);
+
+    setTimeout(function() {
+        const msg = `pizza ${type} ${name} is ready`
+       callback(msg);
+    },3000);
+} 
+
 
 
 
