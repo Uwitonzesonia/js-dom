6c7e3473a8ba7314f392e20e8835e9c9959b4538
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-09-06 00:05:55 +0200

    change

---
+++ react-learning/src/App.jsx
@@ -0,0 +1,5 @@
+import message  from "./message";
+function App() {
+  return <div><message></message></div>
+}
+export default App;
---
+++ react-learning/src/message.tsx
@@ -0,0 +1,7 @@
+// PascalCasing
+function message() {
+   // jsx: javascript xml 
+ return <h1> hello worldw</h1>;
+}
+export default message;
+   
