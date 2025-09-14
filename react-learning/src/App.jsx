a2488f6567a0a44d72c92237b6b3774c62c408b8
Uwitonzesonia <uwitonzesonia2005@gmail.com>
2025-09-14 05:25:21 -0700

    change

--- react-learning/src/App.jsx
+++ react-learning/src/App.jsx
@@ -1,8 +1,30 @@
-function App() {
+import React, {useState} from "react";
 
-  return(
-    <div> <h2 className="red-text">CatPhotoApp</h2> </div>
-    )
+export default function App() {
+const [Count, setCount] = useState(0);
+const increase = () => {
+  setCount(Count + 1)
+};
+const decrease = () => {
+  if (Count > 0){
+    setCount(Count - 1)
+  }
+  
+};
+return (
+    <div style={styles.container}>
+      <h1 style={styles.title}>Counter App</h1>
+      <p style={styles.number}>{count}</p>
+      <div style={styles.buttonContainer}>
+        <button onClick={increase} style={styles.buttonAdd}>
+          ➕ Increase
+        </button>
+        <button onClick={decrease} style={styles.buttonSubtract}>
+          ➖ Decrease
+        </button>
+      </div>
+    </div>
+  );
+
   
 }
-export default App;
